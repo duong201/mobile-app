1b1f1da2415d0ae89ca4bf3da80e8e70d6cc528c
duong201 <truongtheduong88@gmail.com>
2024-01-04 21:40:39 +0700

    build silde

--- components/home/Carousel.jsx
+++ components/home/Carousel.jsx
@@ -1,25 +1,23 @@
 import { View, Text, StyleSheet } from "react-native"
 import { COLOR } from "../../constants"
-// import { SliderBox } from "react-native-image-slider-box"
-// import { COLOR } from "../../constant"
+import { SliderBox } from "react-native-image-slider-box"
 
-const DATA_FAKE = [
-  "https://i.ibb.co/s5mZZc8/slider-3.png",
-  "https://i.ibb.co/7S3s1JJ/slider-2.png",
-  "https://i.ibb.co/n6bjVvy/slider-1.png",
-]
+
 
 export default function Carousel() {
+  const DATA_FAKE = [
+    "https://i.ibb.co/s5mZZc8/slider-3.png",
+    "https://i.ibb.co/7S3s1JJ/slider-2.png",
+    "https://i.ibb.co/n6bjVvy/slider-1.png",
+  ]
+
   return (
     <View style={styles.carouselContainer}>
-      {/* <SliderBox
-        image={DATA_FAKE}
-        dotColor={COLOR.primary}
-        ImageCocomponentStyle={{
-          borderRadius: 15,
-          width: "95%",
-          marginTop: 15,
-        }}></SliderBox> */}
+        <SliderBox
+          images={DATA_FAKE}
+          sliderBoxHeight={180}
+          onCurrentImagePressed={index => console.warn(`image ${index} pressed`)}
+        />
     </View>
   )
 }
